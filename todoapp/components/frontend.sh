37c8a43388ecc0166a079b62edd6a390893f2410
Amrutharaj <amrutha@zelarsoft.com>
2021-06-07 10:16:14 +0530

    ss

--- todoapp/components/frontend.sh
+++ todoapp/components/frontend.sh
@@ -18,7 +18,7 @@ DOWNLOAD_COMPONENT
 
 
 Head "Installing npm"
-npm install &>>$LOG && npm run build &>>$LOG
+npm install -g npm@latest &>>$LOG && npm run build &>>$LOG
 Stat $?
 
 Head "Remove Default Configuration"
