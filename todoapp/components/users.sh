4cb25f9018a811d3018e37b7de6847438ee4b6cf
Amrutharaj <amrutha@zelarsoft.com>
2021-06-07 14:53:54 +0530

    update users

--- todoapp/components/users.sh
+++ todoapp/components/users.sh
@@ -8,6 +8,7 @@ CREATE_DIRECTORY
 
 DOWNLOAD_COMPONENT
 
+
 Head "Installing Java and Maven"
 apt update &>>$LOG && apt install openjdk-8-jre-headless -y &>>$LOG && apt install openjdk-8-jdk-headless -y &>>$LOG
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
@@ -16,8 +17,13 @@ Stat $?
 
 Head "Installing Maven packages"
 mvn clean package &>>$LOG
+mv target/users-1.0.jar users.jar &>>$LOG
 Stat $?
 
-Head "Start service"
-java -jar target/users-api-0.0.1.jar
+Head "Update Service File"
+sed -i -e "s/REDIS_ENDPOINT/redis.$DOMAIN/" /home/todoapp/users/users.service
+Stat $?
+
+Head "Setup SystemD Service"
+mv /home/todoapp/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users &>>$LOG
 Stat $?
