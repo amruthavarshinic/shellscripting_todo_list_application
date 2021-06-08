6aac3aa784c7c8d793a560baab2495ab90894ef6
Amrutharaj <amrutha@zelarsoft.com>
2021-06-08 11:40:26 +0530

    ss

--- todoapp/components/frontend.sh
+++ todoapp/components/frontend.sh
@@ -23,7 +23,7 @@ DOWNLOAD_COMPONENT
 
 Head "Installing npm"
 # npm install &>>$LOG && npm run build  &>>$LOG
-npm install -g npm@latest &>>$LOG && npm audit fix &>>$LOG && npm run build  &>>$LOG
+npm install -g npm@latest & &>>$LOG && npm audit fix &>>$LOG && npm run build  &>>$LOG
 npm rebuild node-sass &>>$LOG
 Stat $?
 
