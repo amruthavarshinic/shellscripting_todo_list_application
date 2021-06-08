b9255408a1a78f43637bde52f41e598e6617e8ce
Amrutharaj <amrutha@zelarsoft.com>
2021-06-08 11:49:40 +0530

    ss

--- todoapp/components/frontend.sh
+++ todoapp/components/frontend.sh
@@ -12,30 +12,24 @@ apt install nginx -y &>>$LOG
 apt install npm -y &>>$LOG
 Stat $?
 
-# Head "Remove Default Configuration"
-# rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
-# service nginx restart
-# Stat $?
-
-cd /var/www/html
+CREATE_DIRECTORY
 
 DOWNLOAD_COMPONENT
 
 Head "Installing npm"
 # npm install &>>$LOG && npm run build  &>>$LOG
-npm install -g npm@latest & &>>$LOG && npm audit fix  & &>>$LOG && npm run build & &>>$LOG
-npm rebuild node-sass & &>>$LOG
+npm install -g npm@latest &>>$LOG && npm audit fix &>>$LOG && npm run build  &>>$LOG
+npm rebuild node-sass &>>$LOG
 Stat $?
 
 Head "configure environmental variables"
-# mv /home/todoapp/frontend/config/index.js /etc/nginx/sites-enabled/todo.conf
+
 sed -i -e '32 s/127.0.0.1/login.zsdevops.online/g' -e '36 s/127.0.0.1/todo.zsdevops.online/g' /home/todoapp/frontend/config/index.js
 # export AUTH_API_ADDRESS=http://login.$DOMAIN:8080
 # export TODOS_API_ADDRESS=http://todo.$DOMAIN:8080
 Stat $?
 
 Head "start NGINX and npm Services"
-# fuser -k 80/tcp
-# nginx -t
+
 systemctl restart nginx 
 npm start
