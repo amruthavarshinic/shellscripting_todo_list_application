e73377a538d8be593e07bb7a9f8bd5e32615d732
Amrutharaj <amrutha@zelarsoft.com>
2021-07-16 08:51:40 +0530

    ss

--- todoapp/components/frontend.sh
+++ todoapp/components/frontend.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 # #Used export instead of service file
-# DOMAIN=zsdevops.online
+DOMAIN=zsdevops.online
 
 OS_PREREQ
 
@@ -18,13 +18,14 @@ DOWNLOAD_COMPONENT
 
 Head "Installing npm"
 # npm install &>>$LOG && npm run build  &>>$LOG
+npm install -g shelljs & &>>$LOG
 npm install -g npm@latest & &>>$LOG && npm run build  &>>$LOG
-npm rebuild node-sass & &>>$LOG
+# npm rebuild node-sass & &>>$LOG
 Stat $?
 
 Head "configure environmental variables"
 
-sed -i -e '32 s/127.0.0.1/login.zsdevops.online/g' -e '36 s/127.0.0.1/todo.zsdevops.online/g' /home/todoapp/frontend/config/index.js
+sed -i -e '32 s/127.0.0.1/login-dev.$DOMAIN/g' -e '36 s/127.0.0.1/todo-dev.$DOMAIN/g' /home/todoapp/frontend/config/index.js
 # export AUTH_API_ADDRESS=http://login.$DOMAIN:8080
 # export TODOS_API_ADDRESS=http://todo.$DOMAIN:8080
 Stat $?
