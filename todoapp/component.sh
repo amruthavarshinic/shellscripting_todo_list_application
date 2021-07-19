f93c087929c8d7933dea347694943f51296b518b
Amrutharaj <amrutha@zelarsoft.com>
2021-07-19 15:59:57 +0530

    ss

--- todoapp/component.sh
+++ todoapp/component.sh
@@ -4,8 +4,8 @@ source components/common.sh
 
 
 ssh 3.230.3.58 " curl -s -L -o /tmp/frontend.zip "https://github.com/zs-amrutha/todoapp-shellscripting/archive/refs/heads/main.zip" "
-unzip /tmp/frontend.zip
-mv frontend-main/* .
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
 
 
 
