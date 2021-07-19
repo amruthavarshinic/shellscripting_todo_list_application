40a54a4f60fc2b456bb07caade9b2029216dc4d1
Amrutharaj <amrutha@zelarsoft.com>
2021-07-19 15:04:29 +0530

    ss

--- todoapp/component.sh
+++ todoapp/component.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-Head " Download the shellscript"
-curl -L -O "https://github.com/zs-amrutha/todoapp-shellscripting/archive/refs/heads/main.zip"
+Head "login to the server"
+ssh 3.230.3.58
+
+Head "Download the shellscript"
+curl -s -L -o /tmp/frontend.zip "https://github.com/zs-amrutha/todoapp-shellscripting/archive/refs/heads/main.zip"
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+
 
 
