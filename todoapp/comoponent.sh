f4f37f9df90d052705f4625b32aac148d845d2ac
Amrutharaj <amrutha@zelarsoft.com>
2021-07-20 12:32:10 +0530

    ss

--- todoapp/comoponent.sh
+++ todoapp/comoponent.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# git config --global credential.helper 'cache --timeout=18000'
+
 USER_NAME=$(whoami)
 
 if [ "${USER_NAME}" != "root" ]; then
@@ -7,4 +9,4 @@ if [ "${USER_NAME}" != "root" ]; then
   exit 1
 fi
 
-ssh 100.26.227.122 " curl -s -L -o /tmp/redis.zip "https://github.com/zs-amrutha/todoapp-shellscripting/archive/main.zip" ; cd todoapp-shellscripting/todoapp ; sudo bash todoapp.sh redis"
+ssh 100.26.227.122 " curl -s -L -o /tmp/todoapp-shellscripting "https://github.com/zs-amrutha/todoapp-shellscripting/archive/main.zip" ; unzip /tmp/todoapp-shellscripting.zip ; mv todoapp-shellscripting-main/* .cd todoapp-shellscripting/todoapp ; sudo bash todoapp.sh redis"
