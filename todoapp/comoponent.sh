cf333737f5898cf2ca356bac3d24b62bf5cecee8
Amrutharaj <amrutha@zelarsoft.com>
2021-07-20 11:38:39 +0530

    ss

--- todoapp/comoponent.sh
+++ todoapp/comoponent.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root" ]; then
+  ERROR "You should be a root user to perform this script, Try with sudo"
+  exit 1
+fi
+
 ssh 100.26.227.122 "curl -O -L https://github.com/zs-amrutha/todoapp-shellscripting/archive/refs/heads/main.zip; c
 d todoapp-shellscripting/todoapp ; sudo bash todoapp.sh redis"
