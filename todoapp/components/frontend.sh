764fea48c854d9471182e5ee208555847cc20720
Amrutharaj <amrutha@zelarsoft.com>
2021-07-26 18:40:56 +0530

    ss

--- todoapp/components/frontend.sh
+++ todoapp/components/frontend.sh
@@ -5,7 +5,6 @@ source components/common.sh
 OS_PREREQ
 
 Head "Installing NGINX and npm"
-apt install nginx -y &>>$LOG
 apt install npm -y &>>$LOG
 Stat $?
 
@@ -21,13 +20,10 @@ npm run build  &>>$LOG
 Stat $?
 
 Head "configure environmental variables"
-sed -i -e 's+/var/www/html+/var/www/html/todo/frontend/dist+g' /etc/nginx/sites-enabled/default
 sed -i -e '32 s/127.0.0.1/login.zsdevops.online/g' -e '36 s/127.0.0.1/todo.zsdevops.online/g' /var/www/html/todoapp/frontend/config/index.js
 Stat $?
 
 Head "start NGINX and npm Services"
-
-systemctl restart nginx 
 mv /var/www/html/todoapp/frontend/systemd.service /etc/systemd/system/frontend.service && systemctl daemon-reload && systemctl start frontend && systemctl enable frontend &>>$LOG
 # npm start
 Stat $?
